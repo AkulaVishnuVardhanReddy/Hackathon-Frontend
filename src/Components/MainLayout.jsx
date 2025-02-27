8547d978e58994460a05e77b133a9853ff9f6164
ramesh <rameshbanalab@gmail.com>
2025-02-27 10:45:00 +0530

    navbar nav

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
+
 const MainLayout = () => {
   return (
     <div className="flex h-screen">
