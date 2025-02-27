5c58d0e526af97d33f103e1f6b00d89e23823cb9
ramesh <rameshbanalab@gmail.com>
2025-02-27 10:34:44 +0530

    navbar

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import Carousel from "./Carousel";
-import NavBar from "./Navbar";
-import { Outlet, useLocation } from "react-router-dom";
-import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
-  const location = useLocation(); // Get the current route
-
-  // Routes where NavBar should NOT be shown
-  const hideNavRoutes = ["/login", "/signup"];
-
   return (
     <div>
-
       <Outlet />
-
     </div>
   );
 };
